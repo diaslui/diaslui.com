72d67488f17fb206116a3732be693b5ef3963564
Pedro Luis Dias <luisp.diias@gmail.com>
2025-12-13 18:43:43 -0300

    feat: add mobile music display and update styles

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,12 @@ const eRefs = {
   listenMusicArtist: ".listen-music-artist",
   listenMusicPlatform: ".listen-music-platform",
   listenMusicAlbumCover: ".listen-music-album-cover",
+
+  mobileMusicDiv: ".mobile-music",
+  mobileMusicSongName: ".mobile-music-song-name",
+  mobileMusicArtist: ".mobile-music-artist",
+  mobileMusicPlatform: ".mobile-music-platform",
+  mobileMusicAlbumCover: ".mobile-music-album-cover",
 };
 
 const refElements = () => {
@@ -15,19 +21,29 @@ const refElements = () => {
 };
 
 const updateMusicInfo = (data) => {
+
+  console.log("Updating music info:", data);
+
   if (!data.playing) {
     domRefs.listenMusicDiv.style.display = "none";
+    domRefs.mobileMusicDiv.style.display = "none";
     return;
   }
-  domRefs.listenMusicDiv.style.display = "block";
 
   domRefs.listenMusicSongName.textContent = data.trackName;
   domRefs.listenMusicArtist.textContent = data.artist;
   domRefs.listenMusicPlatform.textContent = data.platform;
   domRefs.listenMusicAlbumCover.src = data.albumCover;
 
+  domRefs.mobileMusicSongName.textContent = data.trackName;
+  domRefs.mobileMusicArtist.textContent = data.artist;
+  domRefs.mobileMusicPlatform.textContent = data.platform;
+  domRefs.mobileMusicAlbumCover.src = data.albumCover;
+
   domRefs.listenMusicDiv.style.display = "block";
+  domRefs.mobileMusicDiv.style.display = "flex";
 };
+
 
 const sse = () => {
   const eventSource = new EventSource("/lastfm/sse");

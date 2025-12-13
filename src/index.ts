2401a579841d8d77c7ec0d70b4051f1871490b84
Pedro Luis Dias <luisp.diias@gmail.com>
2025-12-13 17:20:27 -0300

    fix: update asset path to use join for correct directory resolution

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { initWorkers } from "./services/services";
 import { staticPlugin } from "@elysiajs/static";
 import servicesRoutes from "./services/routes";
 import routes from "./modules/routes";
+import { join } from "path";
 
 const server = new Elysia()
   .listen(3000, () => {
@@ -14,7 +15,7 @@ const server = new Elysia()
   .use(servicesRoutes)
   .use(
     staticPlugin({
-      assets: "public",
+      assets: join(import.meta.dir, "../public"),
       prefix: "/assets",
     })
   );

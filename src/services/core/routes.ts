4e6d6b2ebf49c374bf82db13b49f308a4c67209a
Pedro Luis Dias <luisp.diias@gmail.com>
2026-01-18 09:45:54 -0300

    chore: update styles + create sitemap.xml

--- src/services/core/routes.ts
+++ src/services/core/routes.ts
@@ -3,6 +3,7 @@ import { countVisitsMiddleware } from "./middlewares.js";
 import { Request, Response } from "express";
 import { splitMD } from "./utils.js";
 import redis from "../../redis.js";
+import fs from "fs";
 
 const router = Router();
 router.use(countVisitsMiddleware);
@@ -71,6 +72,70 @@ router.get("/links", (req: Request, res: Response) => {
     title: "Links",
     description: "Connect with me on various platforms.",
   });
+});
+
+router.get("/sitemap.xml", async (req, res) => {
+  const baseUrl = "https://diaslui.com";
+  const today = new Date().toISOString().split("T")[0];
+
+  let sitemap =
+`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+<url>
+<loc>${baseUrl}/</loc>
+<lastmod>${today}</lastmod>
+<priority>1.0</priority>
+</url>
+
+<url>
+<loc>${baseUrl}/articles</loc>
+<lastmod>${today}</lastmod>
+<priority>0.9</priority>
+</url>
+
+`;
+
+  const articlesRes = await fetch(
+    "https://raw.githubusercontent.com/luiisp/blog-storage-diaslui.com/refs/heads/master/posts/index.json"
+  );
+
+  const articles = await articlesRes.json();
+
+  articles.forEach(article => {
+    const url = `${baseUrl}/read/${article.id}`.toString().trim();
+    sitemap += `<url>
+<loc>${url}</loc>
+<lastmod>${new Date(article.date).toISOString()}</lastmod>
+<priority>0.8</priority>
+</url>
+`;
+  });
+
+  sitemap += `
+  <url>
+  <loc>${baseUrl}/linktree</loc>
+  <lastmod>2026-01-10</lastmod>
+  <priority>0.7</priority>
+  </url>
+
+  <url>
+  <loc>${baseUrl}/about</loc>
+  <lastmod>2026-01-10</lastmod>
+  <priority>0.6</priority>
+  </url>
+  `;
+
+  sitemap += "</urlset>";
+
+  res.setHeader("Content-Type", "application/xml");
+  res.send(sitemap);
+});
+
+router.get("/robots.txt", (req: Request, res: Response) => {
+  const robotsTxt = "User-agent: *\nDisallow:\nSitemap: https://diaslui.com/sitemap.xml\n";
+  res.setHeader("Content-Type", "text/plain");
+  res.send(robotsTxt);
 });
 
 export default router;
